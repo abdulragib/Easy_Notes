c8577ce41a9c695ba77ce98c22326d83c30ed704
Abdul Ragib <70109434+abdulragib@users.noreply.github.com>
2021-12-16 12:55:08 +0530

    updation in firebase done

--- lib/pages/viewnote.dart
+++ lib/pages/viewnote.dart
@@ -3,38 +3,53 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ViewNote extends StatefulWidget {
-
   final Map data;
   final String time;
   final DocumentReference ref;
 
-  ViewNote(this.data,this.time,this.ref, {Key? key}) : super(key: key);
+  ViewNote(this.data, this.time, this.ref, {Key? key}) : super(key: key);
 
   @override
   _ViewNoteState createState() => _ViewNoteState();
 }
 
 class _ViewNoteState extends State<ViewNote> {
-  late String title;
-  late String des;
+   var title;
+   var des;
+  bool edit = false;
+  GlobalKey<FormState> key = GlobalKey<FormState>();
 
   void delete() async {
-
     //delete from db
     await widget.ref.delete();
     Navigator.pop(context);
   }
 
+  void save() async{
+    //TODO: showing any kind of alert that new changes have been saved
+    await widget.ref.update(
+      {'title':title, 'description': des},
+    );
+  }
   @override
   Widget build(BuildContext context) {
+    title=widget.data['title'];
+    des=widget.data['des'];
     return SafeArea(
       child: Scaffold(
+        floatingActionButton: edit
+            ? FloatingActionButton(
+                onPressed: save,
+                child: const Icon(Icons.save_rounded, color: Colors.white70),
+                backgroundColor: Colors.grey[700],
+              )
+            : null,
         body: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+          child: Container(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
@@ -42,49 +57,85 @@ class _ViewNoteState extends State<ViewNote> {
                       onPressed: () {
                         Navigator.of(context).pop();
                       },
-                      child: const Icon(Icons.arrow_back_ios_outlined, size: 24.0),
+                      child:
+                          const Icon(Icons.arrow_back_ios_outlined, size: 24.0),
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(
                           Colors.grey[700],
                         ),
                         padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(horizontal: 25.0, vertical: 8.0),
+                          const EdgeInsets.symmetric(
+                              horizontal: 25.0, vertical: 8.0),
                         ),
                       ),
                     ),
                     //
-                    ElevatedButton(
-                      onPressed: delete,
-                      child: const Icon(
-                        Icons.delete_forever,
-                      ),
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                          Colors.red[300],
+                    Row(children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            edit = !edit;
+                          });
+                        },
+                        child: const Icon(
+                          Icons.edit,
                         ),
-                        padding: MaterialStateProperty.all(
-                          const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.grey[500],
+                          ),
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(
+                                horizontal: 25.0, vertical: 10.0),
+                          ),
                         ),
                       ),
-                    ),
+                      //
+                      const SizedBox(
+                        width: 10.0,
+                      ),
+                      //
+                      ElevatedButton(
+                        onPressed: delete,
+                        child: const Icon(
+                          Icons.delete_forever,
+                        ),
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                            Colors.red[300],
+                          ),
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(
+                                horizontal: 25.0, vertical: 10.0),
+                          ),
+                        ),
+                      ),
+                    ])
                   ],
                 ),
                 const SizedBox(height: 12.0),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                        "${widget.data['title']}",
-                        style: const TextStyle(
-                            fontSize: 32.0,
-                            fontFamily: 'lato',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey),
-                        ),
+                    TextFormField(
+                      decoration: const InputDecoration.collapsed(
+                        hintText: "Title",
+                      ),
+                      style: const TextStyle(
+                          fontSize: 32.0,
+                          fontFamily: 'lato',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey),
+                      initialValue: widget.data['title'],
+                      enabled: edit,
+                      onChanged: (_val) {
+                        title = _val;
+                      },
+                    ),
 
                     //
                     Padding(
-                      padding: const EdgeInsets.only(top:12.0, bottom: 12.0),
+                      padding: const EdgeInsets.only(top: 12.0, bottom: 12.0),
                       child: Text(
                         widget.time,
                         style: const TextStyle(
@@ -98,21 +149,29 @@ class _ViewNoteState extends State<ViewNote> {
                       height: 10.0,
                     ),
                     //
-                    Container(
-                      height: MediaQuery.of(context).size.height*0.75,
-                      child: Text(
-                        "${widget.data['description']}",
-                        style: const TextStyle(
-                            fontSize: 20.0,
-                            fontFamily: 'lato',
-                            color: Colors.grey),
+                    TextFormField(
+                      decoration: const InputDecoration.collapsed(
+                        hintText: "Note Description",
                       ),
+                      style: const TextStyle(
+                          fontSize: 20.0, fontFamily: 'lato', color: Colors.grey),
+                      initialValue:widget.data['description'], //default of this textformfield
+                      enabled: edit, // when we click on edit button then only able to change description data
+                      onChanged: (_val) {
+                        des = _val;
+                      },
+                      maxLines: 20,
                     ),
                   ],
                 )
-              ],),
-            ),),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+//bydefault title and descrption will be null and if we to change only title not description then updation will not done
+//so we need grap original value of  title and description from firebase in scaffold so will not null.
